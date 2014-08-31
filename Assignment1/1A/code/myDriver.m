a484d98f68eea044346c972bb25f499281df79b9
Tanmay <tvrandhavane@gmail.com>
2014-08-31 21:26:11 +0530

    removed wrongly structured question 1 directories

--- Assignment1/1A/code/myDriver.m
+++
@@ -1,9 +0,0 @@
-function myDriver(input)
-addpath('../../common/export_fig/')
-addpath('../../common/')
-
-[inp_img, shrunk_img] = my_shrink(input, 2);
-save_image(inp_img, '../images/inp')
-save_image(shrunk_img, '../images/shrunk_2')
-[~, shrunk_img] = my_shrink(input, 3);
-save_image(shrunk_img, '../images/shrunk_3')
--- Assignment1/1B/code/myDriver.m
+++
@@ -1,7 +0,0 @@
-function myDriver(input)
-addpath('../../common/export_fig/')
-addpath('../../common/')
-
-[inp_img, enlarged_image] = bilinear_enlarge(input);
-save_image(inp_img, '../images/inp');
-save_image(enlarged_image, '../images/enlarged');
