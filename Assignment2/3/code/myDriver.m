4fbe8a9d0988bf4da9a51f80f48e0abb3ce5438a
Tanmay <tvrandhavane@gmail.com>
2014-09-04 20:52:56 +0530

    Question 3 working

--- Assignment2/3/code/myDriver.m
+++ Assignment2/3/code/myDriver.m
@@ -1,4 +1,9 @@
-function [] = myDriver()
-tic;
+function myDriver()
+addpath('../../common/export_fig/')
+addpath('../../common/')
+
 [imageOrig, filteredImage] = patchBased('../images/barbara.mat', 1, 0.5);
-toc;
+save('../images/barbara_inp.mat', 'inp_img');
+save('../images/barbara_sharpened.mat', 'sharpened_img');
+save_image(inp_img, '../images/barbara_inp', 0)
+save_image(filteredImage, '../images/barbara_sharpened', 0)
--- Assignment2/3/code/patchBased.m
+++ Assignment2/3/code/patchBased.m
@@ -1,20 +1,25 @@
-function [imageOrig, filteredImage] = patchBased(filename, h, sigmaPatch)
+function [imageOrig, corruptImage, gaussian_mask, filteredImage] = patchBased(filename, h, sigmaPatch)
 
 load(filename);
 imageOrig = double(imageOrig)/255;
 
+%optimization on original image
 gaussian_filter = fspecial('gaussian', size(imageOrig), 0.66); 
 imageOrig = imfilter(imageOrig, h);
 imageOrig = downsample(imageOrig, 2);
-imageOrig = downsample(imageOrig, 2);
+imageOrig = downsample(imageOrig', 2);
 imageOrig = imageOrig';
-%optimization on original image
+
+%corrupt the image
+range=max(imageOrig(:))-min(imageOrig(:));
+range=0.05*range;
+corruptImage = imnoise(imageOrig,'gaussian', 0 ,range*range);
 
 patchSize = 9;
 windowSize = 25;
 
-x=size(imageOrig,1);
-y=size(imageOrig,2);
+x=size(corruptImage,1);
+y=size(corruptImage,2);
 
 windowOffset = (windowSize - 1)/2;
 patchOffset = (patchSize - 1)/2;
@@ -22,8 +27,9 @@ patchOffset = (patchSize - 1)/2;
 totalOffset = windowOffset + patchOffset;
 
 imagePadded = zeros((patchSize-1) + (windowSize - 1) + x , (patchSize-1) + (windowSize - 1) + y);    
-imagePadded((totalOffset + 1):(x + totalOffset), (totalOffset + 1):(y + totalOffset)) = imageOrig;
+imagePadded((totalOffset + 1):(x + totalOffset), (totalOffset + 1):(y + totalOffset)) = corruptImage;
 
+gaussian_mask = fspecial('gaussian', patchSize, sigmaPatch);
 
 for i=(totalOffset + 1):(x + totalOffset)
 	for j=(totalOffset + 1):(y + totalOffset)
@@ -31,21 +37,22 @@ for i=(totalOffset + 1):(x + totalOffset)
 		j
 		for m=(i - patchOffset):(i + patchOffset)
 			for n=(j - patchOffset):(j + patchOffset)
-				patch_p(((m + 1) -(i - patchOffset)) , ((n + 1) - (j - patchOffset))) = exp(-(imagePadded(m,n)^2)/(2*sigmaPatch^2));
+				patch_p(((m + 1) -(i - patchOffset)) , ((n + 1) - (j - patchOffset))) = imagePadded(m,n) * gaussian_mask(((m + 1) -(i - patchOffset)) , ((n + 1) - (j - patchOffset)));
 			end
 		end
 		filter = zeros(windowSize, windowSize);
 		for k=(i - windowOffset):(i + windowOffset)
 			for l=(j - windowOffset):(j + windowOffset)
+				patch_norm = 0;
 				for m=(k - patchOffset):(k + patchOffset)
 					for n=(l - patchOffset):(l + patchOffset)
-						patch_norm = (exp(-(imagePadded(m, n)^2)/(2*sigmaPatch^2)) - patch_p((m + 1) -(k - patchOffset), ((n + 1) - (l - patchOffset))))^2;
+						patch_norm = patch_norm + (imagePadded(m, n)*gaussian_mask(((m + 1) -(k - patchOffset)), ((n + 1) - (l - patchOffset))) - patch_p(((m + 1) -(k - patchOffset)), ((n + 1) - (l - patchOffset))))^2;
 					end
 				end
  				filter(k - (i - windowOffset - 1), l - (j - windowOffset - 1)) = -sqrt(patch_norm)/(h*h);
 			end
 		end
-		normalize = sum(filter(:));		
+		normalize = sum(filter(:));	
         filter = filter/normalize;
         weights = imagePadded((i - windowOffset):(i + windowOffset), (j - windowOffset):(j + windowOffset));
         weights = weights.*filter;
