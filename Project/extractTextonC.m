069a0e52124f7381e0a49de2c1f1112ce3e89a89
dadlakha <dadlakha0111@gmail.com>
2014-10-24 21:11:34 +0530

    Pre processing

--- Project/extractTextonC.m
+++ Project/extractTextonC.m
@@ -8,19 +8,24 @@ function [texton] = extractTextonC(image, Database)
    width = imageSize(2);
    noOfFilter = size(Database,3);
    texton = zeros(width,height,noOfFilter);
-
+   filterSize = size(Database(:,:,1));
+   fh = filterSize(1);
+   fw = filterSize(2);
+   
    F = zeros(size(Database,3),size(Database,1)*size(Database,2));
     
-    for i=1:size(Database,3)
+   for i=1:size(Database,3)
         A = Database(:,:,i);
         F(i,:) = A(:);
-    end
+   end
 
    for k=1:noOfFilter
        filter = Database(:,:,k);
        size(filter);
        texton(:,:,k) = conv2(image,filter,'same');
-    end
+   end
+   
+   texton = texton(fh+1:height-fh,fw+1:width-fw,:);
 
     % temp=texton(256,256,:);
     % temp=vec2mat(pinv(F)*temp(:),49);
