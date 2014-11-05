bbb39a6e0adbc09d2b7f86150806e9694d9668b8
Tanmay <tvrandhavane@gmail.com>
2014-11-05 23:40:32 +0530

    smoothing me call

--- Assignment5/2/code/smoothing.m
+++ Assignment5/2/code/smoothing.m
@@ -1,29 +1,34 @@
-function [origX, filteredImage] = smoothing(image, spatialSig, intensitySig)
+function [origX, filteredImage] = smoothing(image, spatialSig, intensitySig, windowSize)
     [origX, ~] = imread(image);
     %origX = double(origX)/255;
-    covMatrix = zeros(5, 5);
-    covMatrix(1, 1) = spatialSig*spatialSig;
-    covMatrix(2, 2) = spatialSig*spatialSig;
-    covMatrix(3, 3) = intensitySig*intensitySig;
-    covMatrix(4, 4) = intensitySig*intensitySig;
-    covMatrix(5, 5) = intensitySig*intensitySig;
-    vec = zeros(size(origX, 1)*size(origX, 2), 5);
-    
-    for i = 1:size(origX, 1)
-        for j = 1:size(origX, 2)
-            vec((i-1)*size(origX, 2) + j, 1) = i;
-            vec((i-1)*size(origX, 2) + j, 2) = j;
-            vec((i-1)*size(origX, 2) + j, 3:5) = origX(i, j, :);
+    invCovMatrix = zeros(5, 5);
+    invCovMatrix(1, 1) = 1/(spatialSig*spatialSig);
+    invCovMatrix(2, 2) = 1/(spatialSig*spatialSig);
+    invCovMatrix(3, 3) = 1/(intensitySig*intensitySig);
+    invCovMatrix(4, 4) = 1/(intensitySig*intensitySig);
+    invCovMatrix(5, 5) = 1/(intensitySig*intensitySig);
+    vec = zeros(size(origX, 1), size(origX, 2), 5);
+    newVec = zeros(size(origX, 1), size(origX, 2), 5);
+    offset = (windowSize - 1)/2;
+    for i = offset+1:size(origX, 1)-offset
+        for j = offset+1:size(origX, 2)-offset
+            vec(i, j, 1) = i;
+            vec(i, j, 2) = j;
+            vec(i, j, 3:5) = origX(i, j, :);
         end
     end
     
-    %newVec = meanShift(vec, covMatrix);
-    newVec = vec;
+     
+    for i = offset+1:size(origX, 1)-offset
+        for j = offset+1:size(origX, 2)-offset
+            newVec(i, j, :) = meanShiftClusteringWindowed(vec(i, j, :)', vec, offset, invCovMatrix)';
+        end
+    end
     
     filteredImage = zeros(size(origX, 1),size(origX, 2), 3);
     for i = 1:size(origX, 1)
         for j = 1:size(origX, 2)
-            filteredImage(i, j, :) = vec((i-1)*size(origX, 2) + j, 3:5);
+            filteredImage(i, j, :) = newVec(i, j, 3:5);
         end
     end    
 end
